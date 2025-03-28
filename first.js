d754f0df11c63b53b1c7e0e578b3c5d08f09a88d
heeralalkumarheera <heeralalkumarheera@gmail.com>
2025-03-29 00:06:52 +0530

    testing

---
+++ first.js
@@ -0,0 +1,220 @@
+// console.log('Jai shree ram')
+// var myName='Heeralal Kumar Sahni\n'
+// var college='Government Engineering College \n'
+// var distric='Darbhanga\n'
+// var via='Kusheshwar \n'
+// var mobile=9065241296
+// console.log(myName,college,distric,via,mobile)
+// console.log(typeof(myName))
+// console.log(typeof(mobile))
+// console.log(5 + 10);
+// console.log("Remainder Operator " +27%4);
+// var num=15;
+// var newNum=++num;
+// console.log(num);
+// console.log(newNum);
+// var a=30;
+// var b=20;
+// console.log(a==b);
+// console.log(a!=b);
+//swapping two number without using third variable
+// var a=5;
+// var b=10;
+// a=a+b;
+// b=a-b;
+// console.log(a,b);
+//swapping two number with third variable
+// var a=20;
+// var b=40;
+// var c=b;
+// b=a;
+// a=c;
+// console.log(a,b)
+//  var tomr='rain';
+//  if(tomr=='sunny'){
+//   console.log("take a raincoaat");
+//  }else{
+//   console.log("no need to take rain coat")
+//  }
+//leap year program
+// var leapYear=2021;
+// if(leapYear%4==0 && (leapYear%400==0 || leapYear%100!==0 )){
+//   console.log("This year is leap year")
+// }else{
+//   console.log("not a leap year")
+// }
+//total 5 falsy values in javascript that is 0, " ",undefine,null NaN,faslse** is false
+// if(score=0){
+//   console.log("we loss the game")
+// }else{
+//   console.log("won the game")
+// }
+//condition
+//  var age=17;
+//  if(age>=30){
+//   console.log("eligible to vote")
+//  }else{
+//   console.log("not eligible to vote")
+//  }
+//ternary condition operator where you get the three operands
+// var age=12;
+// console.log((age>=18)? "you can vote" : "You cannot vote")
+
+// while loop
+// var num=0;
+// while(num <=10){
+//   console.log(num);
+//   num++;
+// }
+
+//for loop
+// for(var num=0;num<=10;num++){
+//   console.log(num)
+// }
+
+//table
+// var n=8;
+// for(var num=1;num<=10;num++){
+//   console.log(n*num);
+// }
+
+//table
+// var n=5;
+// for(var a=1;a<=10;a++){
+//   console.log(n+"*" +a+ "=" +n*a);
+// }
+
+//table of 20
+// var n=20;
+// for(var a=1;a<=10;a++){
+//   console.log(n+ "*" +a+ "=" +n*a)
+//  }
+
+//var
+// var myName="Heeralal"
+// console.log(myName)
+
+// function sum(a,b){
+
+//     return total=a+b;
+// }
+// var add=sum(10,20)
+// console.log(add);
+// function sum(a,b){
+//     var total=a+b;
+//     console.log(total)
+// }
+// var sum=sum(10,50)
+//anonymus function
+// var funexp=function(a,b){
+//     return total=a+b;
+// }
+// console.log(funexp(5,10));
+
+// var heera=function(c,d){
+//     return c-d;
+// }
+// console.log(heera(50,40))
+// var heera=function(a,b){
+//     return a*b;
+// }
+// console.log(heera(20,10))
+//var,let and const
+// var myName="Heeralal";
+// myName="lal"
+// myName="Sahni"
+// console.log(myName) //here overridding case is occuranace
+
+//tamplets
+// for(let num=1;num<=10;num++){
+//     let tableOf=10;
+//     // console.log(tableOf + "*" + num + "=" +tableOf*num);
+//     console.log(`${tableOf}*${num}=${tableOf*num}`);
+// }
+// var mydata=['Heera','ramesh','arjun','vishal'];
+// console.log(mydata[1])
+// console.log(mydata.length)
+
+// var heera=['apple','ball','cat','dog']
+// for(let elements in heera){
+//     console.log(elements);
+// }
+
+// let a=[7,3,5,8]
+// a.sort()
+// console.log(a)
+
+// let b = [2, 1, 5, 8, 9, 4, 0, 2, 5, 8, 9, 15];
+// b.sort();
+// console.log(b);
+// let a=[3,5,1,2,8,5,9,0,6,10]
+// a.sort()
+// console.log(a)
+
+// const animals=['eggs','omlet','eggfried'];
+// animals.unshift ('egg curry','egg roll','eff momos');
+// console.log(animals);
+// // animals.splice(2,1,10,20);
+// animals.splice(2,2,11,22);
+// console.log(animals);
+
+//splice method of array
+//  const months=['jan','march','april','june','july'];
+
+// months.splice(1,0,'feb');
+// months.splice(4,0,'may')
+// console.log(months);
+//  months.splice(2,1,'March')
+// console.log(months)
+
+// console.table() method
+console.table({ a: 1, b: 2, c: 3, d: 5, e: 9 });
+
+// console.count() method
+// for(let i=0;i<5;i++){
+//     console.count(i);
+// }
+// console.clear()
+
+// console.warn('Heeralal kumara Sahni')
+// console.error('heerallal')
+
+// Return the product of a and b
+// function Product(a, b) {
+//     return a * b;
+// };
+
+// console.log(Product(6, 10));
+
+// const curDate= new Date();
+// // console.log(curDate.getDate());
+// console.log(curDate.toLocaleDateString());
+// console.log(curDate.getTime());
+// console.log(curDate.getMonth());
+// console.log(curDate.getDay());
+
+// const curTime=new Date();
+// console.log(curTime.getTime());
+
+//  console.log(Math.floor(Math.random()*10));
+
+// obj={a:1,b:2,c:3}
+// console.table()
+
+// alert(location.href);
+// if (confirm("Want to Visit YouTube?")) {
+//   location.href = "https://www.youtube.com";
+// }
+
+// inp = prompt("Hi", "No");
+
+// 1. Variables and Data Types
+var name = "Heeralal Kumar Sahni"; // String
+var age = 25; // Number
+var isStudent = true;
+
+
+
+
+
+
